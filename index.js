1482a86360d2a883b6f703b47106812dc107cc8f
RogiRocz <rogirocz@gmail.com>
2021-11-15 11:36:29 -0300

    add routes

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
+const cors = require("cors")
+const indexRoute = require("./src/Routes/indexRoute")
+const graphPopRoute = require("./src/Routes/GraphPop")
 
-app.get("/", (req, res) => {
-    res.send("Ola mundo")
-})
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.static("./src/Pages"))
+app.use(cors())
 
+app.use("/", indexRoute)
+app.use("/", graphPopRoute)
 app.listen(8080);
+
+console.log("Server is running!");
